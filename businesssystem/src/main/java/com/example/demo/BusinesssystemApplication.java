e6029a9ec5101958989ddf79fd556d33ca26bd06
sa08002 <1078sa08002@gmail.com>
2021-10-03 13:46:26 +0900

    プロジェクト作成

---
+++ businesssystem/src/main/java/com/example/demo/BusinesssystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinesssystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinesssystemApplication.class, args);
+	}
+
+}
